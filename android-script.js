4e83b1e4fff11b88a0693db1eadc44d25d80225f
Joseph Kempster <jkempster34@gmail.com>
2019-09-23 23:30:51 +0100

    Add animated text and image elements

---
+++ android-script.js
@@ -0,0 +1,19 @@
+// window.onload = () => {
+//   const subheader = document.getElementById("subheader");
+
+//   const changeSubheader = () => {
+//     setTimeout(
+//       () => (subheader.innerHTML = "Focus when"),
+//       setTimeout(
+//         () => (
+//           setTimeout(() => (subheader.innerHTML = "Just the way"), 1000),
+//           (subheader.innerHTML = "Dark when")
+//         ),
+//         1000
+//       ),
+//       1000
+//     );
+//   };
+
+//   changeSubheader();
+// };
